82c6e083030062e7f93bfcc370b0dad9609920ee
William Arustamyan <williamarustamyan@gmail.com>
2018-05-14 16:24:57 +0400

    Create user entity.

---
+++ truemotors-core/core-db-entities/src/main/java/com/willenyx/truemotors/core/db/entities/User.java
@@ -0,0 +1,180 @@
+package com.willenyx.truemotors.core.db.entities;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Author: William Arustamyan
+ * Date: 14/05/2018
+ * Time: 4:00 PM
+ */
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "seq_user", sequenceName = "seq_user")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updated;
+
+    @Column(name = "deleted", nullable = true)
+    private LocalDateTime deleted;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created = LocalDateTime.now();
+        this.updated = created;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public LocalDateTime getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(LocalDateTime deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return new EqualsBuilder()
+                .append(id, user.id)
+                .append(firstName, user.firstName)
+                .append(lastName, user.lastName)
+                .append(phoneNumber, user.phoneNumber)
+                .append(email, user.email)
+                .append(created, user.created)
+                .append(updated, user.updated)
+                .append(deleted, user.deleted)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(firstName)
+                .append(lastName)
+                .append(phoneNumber)
+                .append(email)
+                .append(created)
+                .append(updated)
+                .append(deleted)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("phoneNumber", phoneNumber)
+                .append("email", email)
+                .append("created", created)
+                .append("updated", updated)
+                .append("deleted", deleted)
+                .toString();
+    }
+}
