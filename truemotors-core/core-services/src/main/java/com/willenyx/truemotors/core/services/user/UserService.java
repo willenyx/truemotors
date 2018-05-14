2d61319802cbddc9dfc21b69b1c46197ef769a4d
William Arustamyan <williamarustamyan@gmail.com>
2018-05-14 17:54:10 +0400

    User creation service.

---
+++ truemotors-core/core-services/src/main/java/com/willenyx/truemotors/core/services/user/UserService.java
@@ -0,0 +1,21 @@
+package com.willenyx.truemotors.core.services.user;
+
+import com.willenyx.truemotors.core.db.entities.User;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Author: William Arustamyan
+ * Date: 14/05/2018
+ * Time: 5:23 PM
+ */
+public interface UserService {
+
+    User create(@NotNull UserCreateUpdateRequest createRequest);
+
+    User get(long id);
+
+    User update(@NotNull UserCreateUpdateRequest updateRequest);
+
+    void delete(long id);
+}
