081705d6ee591c6c8e38b8bd3086e50daf84725b
William Arustamyan <williamarustamyan@gmail.com>
2018-05-14 17:00:01 +0400

    Configure datasource and flyway.

---
+++ truemotors-core/core-db-repositories/src/main/resources/db/migration/V0_0_1__create_user.sql
@@ -0,0 +1,14 @@
+create sequence seq_user start 1 increment 50;
+
+
+create table user (
+  id           bigint       not null constraint pk_user primary key,
+  first_name   varchar(100) not null,
+  last_name    varchar(100) not null,
+  phone_number varchar(20)  not null,
+  email        varchar(50)  not null,
+  password     varchar(20)  not null,
+  created      timestamp    not null,
+  updated      timestamp    not null,
+  deleted      timestamp
+);
